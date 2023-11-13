fa74932138bcb265de4479fc66c2897dcf5f0091
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2023-11-12 21:10:52 -0500

    finish nav bar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,85 +1,50 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="navigator">
+   <nav>
+    <router-link class="nav-item" to="/">Home</router-link>
+    <router-link class="nav-item" to="/test">School</router-link>
+    <router-link class="nav-item" to="/test">Experience</router-link>
+    <router-link class="nav-item" to="/test">Projects</router-link>
+    <router-link class="nav-item" to="/test">Tutoring</router-link>
+    <router-link class="nav-item" to="/test">Links</router-link>
+    
+  </nav>
+  <router-view/>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
+  color: #2c3e50;
+  
+  
+
+}
+.nav {
+ 
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navigator{
+  align: center;
+  margin-top: -400px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  font-weight: bold;
+  color: #2c3e50;
 }
-
-nav a:first-of-type {
-  border: 0;
+nav a.router-link-exact-active {
+  color: #42b983;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.nav-item {
+    color: rgb(69, 9, 235);
+    text-decoration: none;
+    font-size: 16px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    float: left;
 }
 </style>

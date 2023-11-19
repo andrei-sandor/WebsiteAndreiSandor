40c5731a28c17d84a17ef1bb9dabc39e672ab7fb
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2023-11-18 19:12:00 -0500

    Finish school

--- vue-project/src/views/School.vue
+++ vue-project/src/views/School.vue
@@ -4,12 +4,43 @@
 <img src="../pictures/cje.png" style="width:80%">
 </div>
 
+<div class="cjeInfo">
+<p> Concentration informatique </p>
+</div>
+
 <div class="marianopolis">
 <img src="../pictures/marianopolis.jpg" style="width:80%">
 </div>
 
+<div class="marianopolisInfo">
+<p>
+    <p> Programming in Python </p>
+    <p> Enriched Calculus I </p>
+    <p> Enriched Calculus II </p>
+    <p> Enriched Mechanics </p>
+    <p> Enriched Chemistry I </p>
+    <p> Enriched Biology I </p>
+</p>
+</div>
+
 <div class="mcgill">
 <img src="../pictures/mcgilllogo.jpg" style="width:80%">
+</div>
+
+<div class="McGillInfo">
+<p>
+    
+    <p> ECSE 321: Intro to Software Engineering </p>
+    <p> ECSE 428: Software Engineering Practice </p>
+    <p> ECSE 324: Computer Organization </p>
+    <p> ECSE 202: Intro to Software Development</p>
+    <br>
+    <br>
+    <p> COMP 251: Algorithm and Data Structures </p>
+    <p> COMP 250: Intro to Computer Science </p>
+    <p> COMP 206: Intro to Software Systems </p>
+    
+</p>
 </div>
 
 </template>
@@ -24,20 +55,38 @@
     height: 400px;
 }
 
+.cjeInfo {
+    position: absolute;
+    margin-top: 500px;
+    margin-left: -380px;
+}
+
 .marianopolis {
     position: absolute;
-    top: 220px;
+    top: 270px;
     left: 500px;
     width: 400px;
     height: 400px;
 }
 
+.marianopolisInfo {
+    position: absolute;
+    margin-top: 500px;
+    margin-left: 120px;
+}
+
 .mcgill {
     position: absolute;
-    top: 250px;
+    top: 350px;
     left: 1000px;
     width: 400px;
     height: 400px;
+}
+
+.McGillInfo {
+    position: absolute;
+    margin-top: 500px;
+    margin-left: 570px;
 }
 
 </style>

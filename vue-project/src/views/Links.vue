05d70a97a91bf0b835d63a7af4f813f6b063b5e6
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2023-12-13 23:57:16 -0500

    Created links.vue

---
+++ vue-project/src/views/Links.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div class="linkedinProfile">
+<h2><a href="https://linkedin.com/in/andrei-sandor"> Linkedin Profile </a></h2>
+</div>
+
+<div class="githubProfile">
+<h2><a href="https://github.com/andrei-sandor"> GitHub Profile </a></h2>
+</div>
+
+<div class="email">
+<h2> My email is andrei.sandor@mail.mcgill.ca </h2>
+</div>
+
+
+</template>
+
+
+
+<style>
+
+.linkedinProfile {
+    margin-top: 100px;
+}
+.githubProfile {
+    margin-top: 50px;
+}
+
+.email {
+    margin-top: 50px;
+}
+
+</style>
